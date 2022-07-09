12142495aaedce3e0330d47d076b529e4f3c07cd
dchay <dylanhay@rogers.com>
2022-07-08 21:52:07 -0600

    employee card generated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+
+const Employee = require('./lib/Employee.js');
 // const Manager = require('./lib/Manager.js');
 // const Option = require('./lib/Option.js');
 // const Engineer = require('./lib/Engineer.js');
-const Intern = require('./lib/Intern.js');
+// const Intern = require('./lib/Intern.js');
 
+new Employee().initializeApp();
 // new Manager().getManager();
 // new Option().getOption();
 // new Engineer().getEngineer();
-new Intern().getIntern();
+// new Intern().getIntern();
 
 
 
