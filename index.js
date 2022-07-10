761202a8582a361bab0600b1e5db6bc27f639e18
dchay <dylanhay@rogers.com>
2022-07-10 16:12:49 -0600

    manager, engineer, intern structures reworked

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-
 const Employee = require('./lib/Employee.js');
-// const Manager = require('./lib/Manager.js');
-// const Option = require('./lib/Option.js');
-// const Engineer = require('./lib/Engineer.js');
-// const Intern = require('./lib/Intern.js');
 
-new Employee().getName();
-
-// new Manager().getManager();
-// new Option().getOption();
-// new Engineer().getEngineer();
-// new Intern().getIntern();
+Employee().getName();
 
 
 
