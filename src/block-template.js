154a574bd84edbd554a31e3985a1ad5cb2b31a60
dchay <dylanhay@rogers.com>
2022-07-12 19:15:05 -0700

    style sheet added

--- src/block-template.js
+++ src/block-template.js
@@ -1,14 +1,14 @@
 function createManagerBlock (manager) {
-    const managerBlock = `<div class="layout">
-    <div class="title">
-        <h2 class="name">${manager.name}</h2>
-        <h3 class="role">Manager</h3>
+    const managerBlock = `<div class="manager-block">
+    <div>
+        <h2>${manager.name}</h2>
+        <h3>Manager</h3>
     </div>
-    <body>
+    <div>
         <p>ID: ${manager.id}</p>
         <p>Email: <a href="mailto:${manager.memail}">${manager.email}</a></p>
         <p>Office: ${manager.officeNumber}</p>
-    </body>
+    </div>
 </div>`
 return managerBlock
 };
@@ -17,16 +17,16 @@ function createEngineerBlocks (engineers) {
     let engineerBlocks = ``
     for(let i = 0; i < engineers.length; i++) {
         let engineer = engineers[i]
-        let block = `<div class="layout">
-        <div class="title">
-            <h2 class="name">${engineer.name}</h2>
-            <h3 class="role">Engineer</h3>
+        let block = `<div class="engineer block">
+        <div>
+            <h2>${engineer.name}</h2>
+            <h3>Engineer</h3>
         </div>
-        <body>
+        <div>
             <p>ID: ${engineer.id}</p>
             <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
             <p>Github: <a href="https://www.github.com/${engineer.github}">${engineer.github}</a></p>
-        </body>
+        </div>
     </div>`
     engineerBlocks += block
     }
@@ -37,16 +37,16 @@ function createInternBlocks (interns) {
     let internBlocks = ``
     for(let i = 0; i < interns.length; i++) {
         let intern = interns[i]
-        let block = `<div class="layout">
-        <div class="title">
-            <h2 class="name">${intern.name}</h2>
-            <h3 class="role">Engineer</h3>
+        let block = `<div class="intern block">
+        <div>
+            <h2>${intern.name}</h2>
+            <h3>Intern</h3>
         </div>
-        <body>
+        <div>
             <p>ID: ${intern.id}</p>
             <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
             <p>School: ${intern.school}</p>
-        </body>
+        </div>
     </div>`
     internBlocks += block
     }
--- src/webpage-template.js
+++ src/webpage-template.js
@@ -1,42 +1,30 @@
 function createPage(managerBlock, engineerBlocks, internBlocks) {
-    const pageTemplate = `<!DOCTYPE html>
+  const pageTemplate = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Team</title>
-        <link rel="stylesheet" href="./dist/css/style.css">
+        <title>The Team</title>
+        <link rel="stylesheet" href="./assets/styles.css">
     </head>
     <body>
-        <nav>
-            <h1>My Team</h1>
-        </nav>
+        <hero>
+            <h1>The Team</h1>
+        </hero>
         <main>
-            <div class="manager">
-                <div class="manager-card">
-                    <h2>Manager</h2>
+            <div class="manager-block-par">
                     ${managerBlock}
-                </div>
             </div>
             <div class="employees">
-                <div class="engineers">
-                    <h2>Engineers</h2>
-                    <div class="engineer-cards">
                         ${engineerBlocks}
-                    </div>
-                </div>
-                <div class="interns">
-                    <h2>Interns</h2>
-                    <div class="intern-cards">
                         ${internBlocks}
-                    </div>
                 </div>
             </div>
         </main>
     </body>
-    </html>`
-    return pageTemplate
+    </html>`;
+  return pageTemplate;
 }
 
 module.exports = createPage;
